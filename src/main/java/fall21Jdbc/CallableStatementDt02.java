35634b0317c6f6d10e10741bd9ec8d6eb0eeb277
ibrkorkut07 <http://github.com/ibrkorkut07>
2022-09-22 18:51:06 +0200

    callableStatement2

--- src/main/java/fall21Jdbc/CallableStatementDt02.java
+++ src/main/java/fall21Jdbc/CallableStatementDt02.java
@@ -8,7 +8,7 @@ public class CallableStatementDt02 {
 		
 		Class.forName("oracle.jdbc.driver.OracleDriver");
 		
-		Connection con = DriverManager.getConnection("jdbc:oracle:thin:@//localhost:1521:xe", "hr", "hr");
+		Connection con = DriverManager.getConnection("jdbc:oracle:thin:@//localhost:1521/orcl", "hr", "oracle");
 		
 		Statement st  = con.createStatement();
 		
